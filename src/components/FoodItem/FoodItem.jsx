e2a5e34b7a1edf6c2110d63035ec1976a0bcdc43
Ajith PM <ajithpmdwaraka@gmail.com>
2025-07-02 18:09:48 +0530

    Footer Section Added

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
+import React, { useContext, } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext'
 
 const FoodItem = ({ id, name, price, description, image }) => {
-    
-    const handleAddToCart = () => {
-        // You can implement add to cart functionality here using the id
-        console.log(`Adding item with id: ${id} to cart`);
-    }
-    
+
+    const { cartItem, addToCart, removeFromCart } = useContext(StoreContext);
+
     return (
         <div className='food-item' data-id={id}>
             <div className="food-item-image-container">
                 <img src={image} alt={name} className="food-item-img" />
-                <div className="add-icon" onClick={handleAddToCart}>
-                    <img src={assets.add_icon_white} alt="Add to cart" />
-                </div>
+                {!cartItem[id]
+                    ? <img className='add' onClick={() => addToCart(id)} alt='' src={assets.add_icon_white} />
+                    : <div className="food-item-counter">
+                        <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt='' />
+                        <p>{cartItem[id]}</p>
+                        <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt="" />
+                    </div>
+                }
+
             </div>
             <div className="food-item-info">
                 <div className="food-item-name-rating">
--- src/context/StoreContextProvider.jsx
+++ src/context/StoreContextProvider.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StoreContext } from "./StoreContext";
 import { food_list } from "../assets/assets";
 
 const StoreContextProvider = (props) => {
+
+    const [cartItem, setCartItem] = useState({});
+
+    const addToCart = (itemId) => {
+        if (!cartItem[itemId]) {
+            setCartItem((prev)=>({...prev,[itemId]:1}))
+        }
+        else {
+            setCartItem((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        }
+        
+    }
+
+    const removeFromCart = (itemId) => {
+        setCartItem((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    }
+
+    useEffect(() => {
+        console.log(cartItem);
+    },[cartItem])
     
     const contextValue = {
-        food_list
+        food_list,
+        cartItem,
+        setCartItem,
+        addToCart,
+        removeFromCart
     }
     return (
         <StoreContext.Provider value={contextValue}>
